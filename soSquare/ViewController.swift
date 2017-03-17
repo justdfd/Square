7ec1b5a518c4c9abfbb7293a593fd7465eeb3dba
justDFD <daveski@dave-dombrowskis-macbook-pro.local>
2017-03-17 11:12:03 -0400

    Initial Commit

---
+++ soSquare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soSquare
+//
+//  Created by Dave Dombrowski on 3/17/17.
+//  Copyright © 2017 justDFD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
